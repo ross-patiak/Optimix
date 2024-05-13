fd36272a31cdbfa801f49df93c0b16c2b53ebf56
ross-patiak <ross.patiak@gmail.com>
2024-05-13 15:33:20 -0400

    added spotify oauth plus added clerk and shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,35 @@
 import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { SignInButton, SignOutButton } from "@clerk/nextjs";
+import { currentUser, clerkClient } from "@clerk/nextjs/server";
 
-export default function HomePage() {
+export default async function HomePage() {
+  const user = await currentUser();
+  const controller = new AbortController();
+  const { firstName, id } = user;
+  const clerkResponse = await clerkClient.users.getUserOauthAccessToken(
+    id as string,
+    "oauth_spotify",
+  );
+
+  const accessToken = clerkResponse.data[0]?.token;
+  const spotifyUrl = `https://api.spotify.com/v1/me/playlists`;
+
+  const spotifyResponse = await fetch(spotifyUrl, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  const data = await spotifyResponse.json();
+
+  console.log(data);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-        </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
-        </div>
-      </div>
+    <main className="container">
+      {user != null ? <div>Hi {firstName}</div> : null}
+      <SignInButton>Login</SignInButton>
+      <SignOutButton>Sign out</SignOutButton>
     </main>
   );
 }
